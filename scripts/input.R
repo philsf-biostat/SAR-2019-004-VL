1684d77a5d8636e499f01aa191789c53a3ed21e2
Felipe Figueiredo <philsf79@gmail.com>
2019-10-10 18:40:00 -0300

    gerenciamento de dados básico

--- scripts/input.R
+++ scripts/input.R
@@ -1,5 +1,20 @@
 library(readxl)
 library(data.table)
+library(stringr)
+
 dados <- read_excel("dataset/Resultados TXA Felipe.xlsx", 
                     skip = 1)
 dados <- data.table(dados)
+
+# data management ---------------------------------------------------------
+dados[str_to_lower(`LADO TXA`) == "esq"]$`LADO TXA` <- "Esq"
+dados[str_to_lower(`LADO TXA`) == "dir"]$`LADO TXA` <- "Dir"
+dados$`LADO TXA` <- factor(dados$`LADO TXA`)
+
+dados$txa <- rep(as.numeric(NA), nrow(dados))
+dados$ctr <- rep(as.numeric(NA), nrow(dados))
+
+dados[`LADO TXA` == "Dir"]$txa <- dados[`LADO TXA` == "Dir"]$DIR
+dados[`LADO TXA` == "Esq"]$txa <- dados[`LADO TXA` == "Esq"]$ESQ
+dados[`LADO TXA` == "Dir"]$ctr <- dados[`LADO TXA` == "Dir"]$ESQ
+dados[`LADO TXA` == "Esq"]$ctr <- dados[`LADO TXA` == "Esq"]$DIR
