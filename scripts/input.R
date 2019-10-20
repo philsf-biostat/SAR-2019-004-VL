dbe8dff5ab3713b4dfa7c3d728a85e6ffadde410
Felipe Figueiredo <philsf79@gmail.com>
2019-10-20 19:16:20 -0300

    dataset de face renomeado

--- scripts/input.R
+++ scripts/input.R
@@ -2,19 +2,23 @@ library(readxl)
 library(data.table)
 library(stringr)
 
-dados <- read_excel("dataset/Resultados TXA Felipe.xlsx", 
-                    skip = 1)
-dados <- data.table(dados)
+face <- read_excel("dataset/Resultados TXA Felipe.xlsx",
+                   sheet = "face", skip = 1)
+face <- data.table(face)
 
 # data management ---------------------------------------------------------
-dados[str_to_lower(`LADO TXA`) == "esq"]$`LADO TXA` <- "Esq"
-dados[str_to_lower(`LADO TXA`) == "dir"]$`LADO TXA` <- "Dir"
-dados$`LADO TXA` <- factor(dados$`LADO TXA`)
+face[str_to_lower(`LADO TXA`) == "esq"]$`LADO TXA` <- "Esq"
+face[str_to_lower(`LADO TXA`) == "dir"]$`LADO TXA` <- "Dir"
+face$`LADO TXA` <- factor(face$`LADO TXA`)
 
-dados$txa <- rep(as.numeric(NA), nrow(dados))
-dados$ctr <- rep(as.numeric(NA), nrow(dados))
+face$txa <- rep(as.numeric(NA), nrow(face))
+face$ctr <- rep(as.numeric(NA), nrow(face))
 
-dados[`LADO TXA` == "Dir"]$txa <- dados[`LADO TXA` == "Dir"]$DIR
-dados[`LADO TXA` == "Esq"]$txa <- dados[`LADO TXA` == "Esq"]$ESQ
-dados[`LADO TXA` == "Dir"]$ctr <- dados[`LADO TXA` == "Dir"]$ESQ
-dados[`LADO TXA` == "Esq"]$ctr <- dados[`LADO TXA` == "Esq"]$DIR
+face[`LADO TXA` == "Dir"]$txa <- face[`LADO TXA` == "Dir"]$DIR
+face[`LADO TXA` == "Esq"]$txa <- face[`LADO TXA` == "Esq"]$ESQ
+face[`LADO TXA` == "Dir"]$ctr <- face[`LADO TXA` == "Dir"]$ESQ
+face[`LADO TXA` == "Esq"]$ctr <- face[`LADO TXA` == "Esq"]$DIR
+
+# trim cols ---------------------------------------------------------------
+
+face <- face[, .(SEQ, DATA, DIR, ESQ, LADO=`LADO TXA`, Citometria, txa, ctr)]
