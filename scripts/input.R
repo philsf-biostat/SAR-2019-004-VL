3c4ad7493ce4566b111f1dc7fc8344853aec9521
Felipe Figueiredo <philsf79@gmail.com>
2019-10-20 19:17:11 -0300

    dataset de proteses gerenciado

--- scripts/input.R
+++ scripts/input.R
@@ -19,6 +19,25 @@ face[`LADO TXA` == "Esq"]$txa <- face[`LADO TXA` == "Esq"]$ESQ
 face[`LADO TXA` == "Dir"]$ctr <- face[`LADO TXA` == "Dir"]$ESQ
 face[`LADO TXA` == "Esq"]$ctr <- face[`LADO TXA` == "Esq"]$DIR
 
+# protese -----------------------------------------------------------------
+
+protese <- read_excel("dataset/Resultados TXA Felipe.xlsx",
+                      sheet = "protese", skip = 1)
+protese <- data.table(protese)
+
+protese[str_to_lower(`LADO TXA`) == "esq"]$`LADO TXA` <- "Esq"
+protese[str_to_lower(`LADO TXA`) == "dir"]$`LADO TXA` <- "Dir"
+protese$`LADO TXA` <- factor(protese$`LADO TXA`)
+
+protese$txa <- rep(as.numeric(NA), nrow(protese))
+protese$ctr <- rep(as.numeric(NA), nrow(protese))
+
+protese[`LADO TXA` == "Dir"]$txa <- protese[`LADO TXA` == "Dir"]$DIR
+protese[`LADO TXA` == "Esq"]$txa <- protese[`LADO TXA` == "Esq"]$ESQ
+protese[`LADO TXA` == "Dir"]$ctr <- protese[`LADO TXA` == "Dir"]$ESQ
+protese[`LADO TXA` == "Esq"]$ctr <- protese[`LADO TXA` == "Esq"]$DIR
+
 # trim cols ---------------------------------------------------------------
 
 face <- face[, .(SEQ, DATA, DIR, ESQ, LADO=`LADO TXA`, Citometria, txa, ctr)]
+protese <- protese[, .(SEQ, DATA, DIR, ESQ, LADO=`LADO TXA`, COR=COLORAÇÃO, txa, ctr)]
