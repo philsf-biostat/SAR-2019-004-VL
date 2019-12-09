f7887f47428f5f44be35d6346784448df11395b1
Felipe Figueiredo <philsf79@gmail.com>
2019-12-09 10:47:13 -0300

    tabela descritiva (dados a ser incluídos)

--- scripts/results.R
+++ scripts/results.R
@@ -1,4 +1,12 @@
 source('scripts/input.R')
 library(atable)
 
+tabela.desc <- atable::atable(protese, target_cols = c(
+  "Sexo",
+  "Idade",
+  "Altura",
+  "Peso",
+  "IMC"
+), format_to = "Console")
+
 t.test(dreno ~ group, protese.long, paired = TRUE)
